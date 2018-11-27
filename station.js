45bcddf0b630363f47f423cb62b6f34746c4831a
qureeb hameed <quareebs@yahoo.com>
2018-11-27 12:34:09 +0200

    Tested event subscription logic on FASTory simulator. Worked as expectedgit status

--- station.js
+++ station.js
@@ -1,17 +1,19 @@
 let request = require('request');
 let express = require('express');
 let app = express();
-let myIP = "http://192.168.3.100"
-    //let http = require('http').Server;
-    //let socket = require('socketio');
+let bodyParser = require('body-parser').json({ strict: false });
+//let myIP = "192.168.3.100"
+let myIP = '127.0.0.1';
+//let http = require('http').Server;
+//let socket = require('socketio');
 
 var help = require('./helpers');
 
 class Station {
     constructor(name, ip, eventPort) {
         this.name = name;
-        this.ip = ip;
-        this.eventPort = eventPort;
+        this.ip = ip; // ip of the real station
+        this.eventPort = eventPort; // for instances of this object
 
         this.events = undefined; // events array
         this.eventCount = 0; // num of successful subscriptions
@@ -26,51 +28,25 @@ class Station {
         //this.nEvents
     }
 
-    //run a server
-    runServer() {
-        var ref = this;
-        app.post('/', function(req, res) { // for event notifications
-            // on request, emit an event with the eventID and data. different for each station.
-            res.end();
-        });
-
-        app.listen(ref.eventPort, function() {
-            console.log(ref.name, 'is listening on port', ref.eventPort);
-        });
-    }
-
-    getInputs(obj) { // specify inputs as {"matPush":undefined, "swivel":undefined}
+    // specify inputs as {"matPush":undefined, "swivel":undefined}
+    getInputs(obj) {
         var ref = this;
         ref.inputs = obj;
-        ref.nInputs = help.getSize(obj); // creating new obj ppty////////////////////////////////
+        ref.nInputs = help.getSize(obj);
     };
 
-    getOutputs(obj) { // specify outputs as {"matPush":undefined, "swivel":undefined}
+    // specify outputs as {"matPush":undefined, "swivel":undefined}
+    getOutputs(obj) {
         var ref = this;
         ref.outputs = obj;
-        ref.nOutputs = help.getSize(obj); // creating new obj ppty////////////////////////////////
+        ref.nOutputs = help.getSize(obj);
     };
 
-    getEvents(arr) { // specify eventIDs as ['xChanged', 'yChanged']
+    // specify eventIDs as ['xChanged', 'yChanged']
+    getEvents(arr) {
         var ref = this;
         ref.events = arr;
-        ref.nEvents = arr.length; // creating new obj ppty////////////////////////////////
-    }
-
-    //make subscriptions OPTION 1
-    subscribe(baseURI, ...events) { // a variable number of events
-
-        var ref = this;
-        baseURI = "http://" + ref.ip + "/rest/events/";
-
-        events.forEach(element => { //subscribe             //////////////////// BAD IDEA? //ASYNC-AWAIT??
-            var uri = baseURI + element + "/notifs";
-            var destUrl = "http://" + ref.ip + ":" + ref.eventPort; // event notifs returned here!
-            request.post({ uri: uri, json: true, body: { destUrl: destUrl } }, function(err, res, body) {
-                if (!err) console.log(element, res.statusCode);
-                else console.log(element, 'error', '    statusCode:', res.statusCode);
-            });
-        });
+        ref.nEvents = arr.length;
     }
 
     //make a POST request
@@ -85,20 +61,20 @@ class Station {
         })
     }
 
-    //make subscriptions OPTION 2
-    subscribe2(baseURI, events) { //events as an array of strings
+    //make subscriptions
+    subscribe(baseURI) {
         var ref = this;
         //baseURI = "http://" + ref.ip + "/rest/events/";
 
-        //var nEvents = events.length;
-        var uri = baseURI + events[ref.eventCount] + "/notifs";
+        var uri = baseURI + ref.events[ref.eventCount] + "/notifs";
         var body = { destUrl: "http://" + myIP + ":" + ref.eventPort };
         ref.makePostRequest(uri, body)
-            .then(function(data) { // data is the response code
+            .then(function(data) { // data is the response code to the most recent subscription
                 if (data.toString().substr(0, 1) == 2) { // success = 2xx
+                    console.log(ref.events[ref.eventCount], 'SUBSCRIBED!');
                     ref.eventCount++;
-                    if (ref.eventCount < ref.nEvents - 1) { /////////////////////////////////////////////////
-                        return ref.subscribe2(baseURI, events); // recursive
+                    if (ref.eventCount < ref.nEvents) {
+                        return ref.subscribe(baseURI); // recursive
                     }
                 }
             })
@@ -107,8 +83,7 @@ class Station {
             });
     }
 
-    //get the initial statuses of all IOs
-    // 2nd OPTION: Manually set initial IO values, OR...//////////////////////////////////////////////////////
+    //get the initial statuses of all inputs
     initInputs(baseURI) { // initialize inputs
         var ref = this;
         //var inp = ref.inputs;
@@ -119,16 +94,19 @@ class Station {
             .then(function(data) {
                 if (data.toString().substr(0, 1) == 2) {
                     ref.inputCount++;
-                    if (ref.inputCount < nInputs - 1) { /////////////////////////////////////////////////
+                    if (ref.inputCount < ref.nInputs) {
                         return ref.initInputs(baseURI); // recursive
                     }
                 }
+            })
+            .catch(function(err) {
+                console.error(err);
             });
     }
 
+    //get the initial statuses of all outputs
     initOutputs(baseURI) { // initialize inputs
         var ref = this;
-        //var inp = ref.inputs;
         //baseURI = "http://" + ref.ip + "/rest/services/";
         uri = baseURI + ref.inputs[ref.inputCount];
         var body = {};
@@ -136,11 +114,30 @@ class Station {
             .then(function(data) {
                 if (data.toString().substr(0, 1) == 2) {
                     ref.inputCount++;
-                    if (ref.inputCount < nInputs - 1) { /////////////////////////////////////////////////
+                    if (ref.inputCount < ref.nOutputs) {
                         return ref.initInputs(baseURI); // recursive
                     }
                 }
+            })
+            .catch(function(err) {
+                console.error(err);
             });
+    }
+
+    //run a server
+    runServer() {
+        var ref = this;
+        app.use(bodyParser);
+        app.post('/', function(req, res) { // for event notifications
+            // on request, emit an event with the eventID and data. different for each station.
+            console.log(req.body);
+
+            res.end();
+        });
+
+        app.listen(ref.eventPort, function() {
+            console.log(ref.name, 'is listening on port', ref.eventPort);
+        });
     }
 }
 
