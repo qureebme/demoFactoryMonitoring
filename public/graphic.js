7bc8225ee9b47a91bd73c97111f5a4425af248c2
Qureeb Hameed <quareebs@yahoo.com>
2019-02-18 18:11:42 +0200

    processing station's spinner created!

--- public/graphic.js
+++ public/graphic.js
@@ -8,10 +8,13 @@ $(document).ready(function() {
     y = 0.1 * h;
     width = 0.8 * w; // 80% of available space
     height = 0.8 * h;
+
+    /*
     console.log("width:", width)
     console.log("height:", height)
     console.log("width:", w)
     console.log("height:", h)
+    */
 
     var s_width = 250; // station width
     var s_height = 150; //station height
@@ -56,23 +59,23 @@ $(document).ready(function() {
     //s.circle(c_x, c_y, 10);
 
     //UPPER SIDE
-    c_y2 = Number($("#cnvBig").attr("y")) - 160; // - 10(spacing) - height_of_asrs// for asrs
+    c_y2 = Number(s.select("#cnvBig").attr("y")) - 160; // - 10(spacing) - height_of_asrs// for asrs
     var asrs = s.rect(c_x - 150, c_y2 - 100, 300, 250).attr({
         id: "asrs",
         fill: "#90A4AE"
     }); //asrs station
 
     //var c_x3 = ;
-    var robs = s.rect(x + width - 40 - s_width, $("#cnvBig").attr("y") - 160, s_width, s_height).attr({ //mv dw hr
+    var robs = s.rect(x + width - 40 - s_width, s.select("#cnvBig").attr("y") - 160, s_width, s_height).attr({ //mv dw hr
         id: "robs",
         fill: "#90A4AE"
     }); // y + height / 2 - Number($("#bigRect").attr("height")) / 2 - 10 - s_height
 
-    var asse = s.rect($("#robs").attr("x"), $("#robs").attr("y") - s_height - 5, s_width, s_height).attr({
+    var asse = s.rect(s.select("#robs").attr("x"), s.select("#robs").attr("y") - s_height - 5, s_width, s_height).attr({
         id: "asse",
         fill: "#90A4AE"
     });
-    var stor = s.rect($("#asse").attr("x"), $("#asse").attr("y") - s_height - 5, s_width, s_height).attr({
+    var stor = s.rect(s.select("#asse").attr("x"), s.select("#asse").attr("y") - s_height - 5, s_width, s_height).attr({
         id: "stor",
         fill: "#90A4AE"
     });
@@ -84,51 +87,113 @@ $(document).ready(function() {
         fill: "#90A4AE"
     }); // handling station
 
-    var proc = s.rect($("#hand").attr("x"), Number($("#hand").attr("y")) + Number($("#hand").attr("height")) + 5,
+    var proc = s.rect(s.select("#hand").attr("x"), Number($("#hand").attr("y")) + Number($("#hand").attr("height")) + 5,
         s_width, s_height).attr({
         id: "proc",
         fill: "#90A4AE"
     }); // processing station
 
     //var c_y3 = ;// for test and dist
-    var test = s.rect(Number($("#cnvBig").attr("x")) + 40, c_y1, s_width, s_height).attr({
+    var test = s.rect(Number(s.select("#cnvBig").attr("x")) + 40, c_y1, s_width, s_height).attr({
         id: "test",
         fill: "#90A4AE"
     });
 
-    var dist = s.rect($("#test").attr("x"), Number($("#test").attr("y")) + Number($("#test").attr("height")) + 5,
+    var dist = s.rect(s.select("#test").attr("x"), Number(s.select("#test").attr("y")) + Number(s.select("#test").attr("height")) + 5,
         s_width, s_height).attr({
         id: "dist",
         fill: "#90A4AE"
     }); // distribution station
 
+    var all = s.group(cnvBig, cnvSm, hand, proc, test, dist, asrs, robs, asse, stor);
+    var cnv = s.group(cnvBig, cnvSm).attr({}); // the conveyor
+
+    var procc = s.circle(Number(s.select("#proc").attr("x")) + 100, Number(s.select("#proc").attr("y")) + 75, 65).attr({
+            fill: "#ffffff",
+            opacity: 0,
+            id: "procc"
+        }),
+
+        procr1 = s.rect(Number(s.select("#proc").attr("x")) + 90, Number(s.select("#proc").attr("y")) + 10, 20, s.select("#procc").attr("r") * 2).attr({
+            fill: "#ffffff",
+            rx: 7,
+            ry: 7,
+            stroke: "#B00020",
+            strokeWidth: 2,
+            id: "procr1"
+        }),
+
+        procr2 = s.rect(Number(s.select("#proc").attr("x")) + 90, Number(s.select("#proc").attr("y")) + 10, 20, s.select("#procc").attr("r") * 2).attr({
+            fill: "#ffffff",
+            rx: 7,
+            ry: 7,
+            stroke: "#6200EE",
+            strokeWidth: 2,
+            id: "procr2"
+        }),
+
+        procr3 = s.rect(Number(s.select("#proc").attr("x")) + 90, Number(s.select("#proc").attr("y")) + 10, 20, s.select("#procc").attr("r") * 2).attr({
+            fill: "#ffffff",
+            rx: 7,
+            ry: 7,
+            stroke: "#03DAC5",
+            strokeWidth: 2,
+            id: "procr3"
+        }),
+        ccc = s.circle(Number(s.select("#proc").attr("x")) + 100, Number(s.select("#proc").attr("y")) + 75, 30).attr({
+            fill: "#FFFFFF",
+            opacity: 1,
+            stroke: "#50FF50",
+            strokeWidth: 2
+        })
 
 
     // GROUPS
-    var all = s.group(cnvBig, cnvSm, hand, proc, test, dist, asrs, robs, asse, stor);
-    var cnv = s.group(cnvBig, cnvSm).attr({}); // the conveyor
-    all.animate({
-            y: "+=200"
-        }, 5000)
-        //grid, upper line
+
+    s.select("#procr1").transform("r60"); // 60 deg off procr1
+    s.select("#procr3").transform("r120"); // 120 deg off procr1
+
+    var spinner = s.group(procr1, procr2, procr3, ccc).attr({
+        id: "spinner"
+    }); // make the spinner whole
+
+    //VISIT THIS LATER
+    /*
+        procc.attr({
+            mask: spinner
+        })
+        */
+    spinner.animate({ transform: "r60" }, 0) // turn spinner 60 deg, duration must be included
+
+    //grid, upper line
     var r = 10;
-    s.circle(x, y, r);
-    s.circle(x + width / 4, y, r);
-    s.circle(x + width / 2, y, r);
-    s.circle(x + width * 0.75, y, r);
-    s.circle(x + width, y, r);
+    var gridLine1 = s.group(
+        s.circle(x, y, r),
+        s.circle(x + width / 4, y, r),
+        s.circle(x + width / 2, y, r),
+        s.circle(x + width * 0.75, y, r),
+        s.circle(x + width, y, r)
+    )
+
 
     //grid, middle line
-    s.circle(x, y + height / 2, r);
-    s.circle(x + width / 4, y + height / 2, r);
-    s.circle(x + width / 2, y + height / 2, r);
-    s.circle(x + width * 0.75, y + height / 2, r);
-    s.circle(x + width, y + height / 2, r);
+    var gridLine2 = s.group(
+        s.circle(x, y + height / 2, r),
+        s.circle(x + width / 4, y + height / 2, r),
+        s.circle(x + width / 2, y + height / 2, r),
+        s.circle(x + width * 0.75, y + height / 2, r),
+        s.circle(x + width, y + height / 2, r)
+    )
 
     //grid, lower line
-    s.circle(x, y + height, r);
-    s.circle(x + width / 4, y + height, r);
-    s.circle(x + width / 2, y + height, r);
-    s.circle(x + width * 0.75, y + height, r);
-    s.circle(x + width, y + height, r);
+    var gridLine3 = s.group(
+        s.circle(x, y + height, r),
+        s.circle(x + width / 4, y + height, r),
+        s.circle(x + width / 2, y + height, r),
+        s.circle(x + width * 0.75, y + height, r),
+        s.circle(x + width, y + height, r)
+    )
+    gridLine1.attr({ opacity: 0 });
+    gridLine2.attr({ opacity: 0 });
+    gridLine3.attr({ opacity: 0 });
 });
